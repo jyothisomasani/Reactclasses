f02a8c36416d821e9800a9a5ee31286ac63716a5
jyothi79 <jyothisomasani@gmail.com>
2023-03-26 20:13:31 -0500

    FNew Commit

--- src/Login.jsx
+++ src/Login.jsx
@@ -3,25 +3,31 @@ export const Login= () => {
     const [email,setemail ]=useState('')
     const [FirstName,setFirstName]=useState('')
     const[LastName,setLastName]=useState('')
+    const handleSubmit=(e)=>{
+        e.preventDefault();
+        console.log(email); }
     return(
         <div>
         <h1>Login Page</h1>
-        <form>
+        <form onSubmit={handleSubmit}
+        >
             <label for="FirstName">First Name : </label>
-            <input  value ={FirstName}type="text" id="FirstName" placeholder="Enter First name"></input>
+            <input  value ={FirstName} onChange ={(e)=>setFirstName(e.target.value)}type="text" id="FirstName" placeholder="Enter First name"></input>
             <br></br>
             <br></br>
             <label for="Last Name">Last Name : </label>
-            <input  value={LastName}type="text" id=":Last Name" placeholder="Enter Last Name"></input>
+            <input  value={LastName} onChange ={(e)=>setLastName(e.target.value)}type="text" id=":Last Name" placeholder="Enter Last Name"></input>
             <br></br>
             <br></br>
             <label for="Email">Email : </label>
-            <input  value ={email}type="email" id="Email" placeholder="Enter Email id"></input>
+            <input  value ={email} onChange ={(e)=>setemail(e.target.value)}type="email" id="Email" placeholder="Enter Email id"></input>
             <br></br>
             <br></br>
-            <input type="submit" value="Submit"/>
+            <button type="submit"> Login</button>
 
         </form>
+        <br></br>
+        <button>Don't have an account ? Register Here</button>
         </div>
     )
 }
